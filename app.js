44e2362c56103dc20a1ad31278dd9ad86ec5a1af
paulthegeek <pj2454@gmail.com>
2016-04-13 16:23:33 -0600

    Added CORS and wrap in closure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require("./app/config");
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Running server on port ' + server.address().port);
--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,56 +1,60 @@
-var Note = require("../models/note");
-var express = require("express");
-var router = express.Router();
-var _ = require("lodash");
+(function() {
+  var Note = require("../models/note");
+  var express = require("express");
+  var app = express();
+  var router = express.Router();
+  var _ = require("lodash");
 
-router.get("/", function(request, response) {
-  response.json({ message: "Welcome to the Note API!" });
-});
+  setupCORS();
 
-router.route("/notes")
+  router.get("/", function(request, response) {
+    response.json({ message: "Welcome to the Note API!" });
+  });
+
+  router.route("/notes")
   .post(function(request, response) {
     var note = new Note();
     note.body = request.body.body;
 
-    note.save(function(err) {
-      if(err) {
-        response.send(err);
+    note.save(function(error) {
+      if(error) {
+        response.send(error);
       }
       response.json({ message: "A saved note..." });
     });
   })
 
   .get(function(request, response) {
-    Note.find(function(err, notes) {
-      if(err) {
-        response.send(err);
+    Note.find(function(error, notes) {
+      if(error) {
+        response.send(error);
       }
       if(_.isEmpty(request.query)) {
         response.json(notes);
       } else {
         Note.find({ body: new RegExp(
-          request.query['q'], 'i') }, function(err, notes) {
-          if(err) {
-            response.send(err);
-          } else {
-            response.json(notes);
-          }
-        });
+          request.query['q'], 'i') }, function(error, notes) {
+            if(error) {
+              response.send(error);
+            } else {
+              response.json(notes);
+            }
+          });
       }
     });
   });
 
-router.route("/notes/:note_id")
+  router.route("/notes/:note_id")
   .put(function(request, response) {
-    Note.findById(request.params.note_id, function(err, note) {
-      if(err) {
-        response.send(err);
+    Note.findById(request.params.note_id, function(error, note) {
+      if(error) {
+        response.send(error);
       }
       note.body = request.body.body;
 
-      note.save(function(err) {
-        if(err) {
-          response.send(err);
+      note.save(function(error) {
+        if(error) {
+          response.send(error);
         }
 
         response.json({ message: "Note updated..." });
@@ -59,12 +63,25 @@ router.route("/notes/:note_id")
   })
 
   .get(function(request, response) {
-    Note.findById(request.params.note_id, function(err, note) {
-      if(err) {
-        response.send(err);
+    Note.findById(request.params.note_id, function(error, note) {
+      if(error) {
+        response.send(error);
       }
       response.json(note);
     });
   });
 
-module.exports = router;
+  function setupCORS() {
+    router.use(function(request, response, next) {
+      response.header("Access-Control-Allow-Origin", "*");
+      response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      response.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+    });
+  }
+
+  module.exports = router;
+})();
